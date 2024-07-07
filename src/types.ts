d3809a06f21f937a4a273dc73d2d47beb1b5ed5b
Steele Valles <steele.valles@kiv.dev>
2024-07-07 19:13:34 +0300

    types added

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+import {Request} from 'express';
+
+export type RequestWithBody<T> = Request<{}, {}, T>;
+export type RequestWithQuery<T> = Request<{}, {}, {}, T>;
+export type RequestWithParams<T> = Request<T>;
+export type RequestWithParamsAndBody<T, I> = Request<T, {}, I>;
+
