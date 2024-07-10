8c5307a25efc82b9e1e366c8dcf4f5c968f46a4b
Steele Valles <steele.valles@kiv.dev>
2024-07-10 13:03:53 +0300

    auth was removed

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import expressBasicAuth from 'express-basic-auth';
 
 import { getUsersRouter } from './routes/users';
 import { getTestsRouter } from './routes/tests';
@@ -8,12 +7,6 @@ import bodyParser from 'body-parser';
 export const app = express();
 
 app.use(bodyParser.json());
-
-app.use(
-  expressBasicAuth({
-    users: { admin: 'qwerty' },
-  }),
-);
 
 app.use('/users', getUsersRouter());
 app.use('/__test__', getTestsRouter());
